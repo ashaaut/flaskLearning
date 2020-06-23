96946326b1fc996527a502467746ad2ab4c9c707
ashaaut <ashaautade1998@gmail.com>
2020-06-23 12:31:02 +0530

    used url_for dynamically building url

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-from flask import  Flask,render_template
+from flask import  Flask,redirect,url_for
 app=Flask(__name__)
 
-@app.route('/hello/')
-def Hello_world():
-    return render_template('index.html') 
+@app.route('/admin/')
+def Hello_admin():
+    return "hello admin" 
 
+@app.route('/guest/<guest>')
+def Hello_guest(guest):
+    return "Hello %s as guest "%guest
+
+@app.route('/user/<name>')
+def Hello_user(name):
+    if name=="admin":
+       return redirect(url_for("Hello_admin"))
+    else:
+       return redirect(url_for("Hello_guest", guest=name))
 if __name__=='__main__':
     app.run(debug=True)
